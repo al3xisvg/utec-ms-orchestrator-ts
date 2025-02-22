4fc100e9f4605120d47c740ae6363b97acd14d5a
al3xisvg <vasquezgarciaalexis@gmail.com>
2025-02-21 20:27:03 -0500

    [update] fix name

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -57,7 +57,7 @@ router.get(
 )
 
 router.get(
-  '/obtain/requirements-with-products/:requirementId',
+  '/obtain/requirement-with-products/:requirementId',
   async (req: Request, res: Response): Promise<void> => {
     try {
       const { requirementId } = req.params
